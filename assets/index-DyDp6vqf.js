5bec5b9237ab9bae3f0f3d720e4834802cad3730
amritnair <amritnair@users.noreply.github.com>
2026-08-21 20:40:52 +0000

    deploy: e83b4f81b82f4a34e37840a025aebda01610cef3

--- assets/index-DyDp6vqf.js
+++ assets/index-DyDp6vqf.js
@@ -24043,7 +24043,7 @@ Best regards`);
         ]
       })
     });
-  }, vz = b.lazy(() => VA(() => import("./GamePortfolio-CoJZ9YwK.js").then(async (m) => {
+  }, vz = b.lazy(() => VA(() => import("./GamePortfolio-B5FVH52M.js").then(async (m) => {
     await m.__tla;
     return m;
   }).then((t) => t.aR), [])), bz = new dD(), xz = () => w.jsx("div", {
