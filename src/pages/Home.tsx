2843bd8883287c5663711bee19ad653a2b2d6e9a
amritnair <amritnair23@gmail.com>
2026-09-22 03:53:23 -0500

    Add research, Tidal, ShotSensei, and hackathon photos to the hero carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -67,6 +67,30 @@ const PHOTOS = [
     caption: "demoing pulsematic",
     position: "50% 46%",
   },
+  {
+    src: "photos/research.jpg",
+    alt: "Amritaraj with his cattle-nutrition research poster at Texas A&M",
+    caption: "research poster, texas a&m",
+    position: "32% 28%",
+  },
+  {
+    src: "photos/tidal.jpg",
+    alt: "Amritaraj and teammates with the TidalTAMU mascot",
+    caption: "tidaltamu 2026",
+    position: "50% 42%",
+  },
+  {
+    src: "photos/shotsensei.jpg",
+    alt: "Amritaraj demoing ShotSensei with a pickleball paddle",
+    caption: "shotsensei, demo day",
+    position: "55% 38%",
+  },
+  {
+    src: "photos/hackathon.jpg",
+    alt: "Amritaraj and teammates at Hook'em Hacks 2026",
+    caption: "hook'em hacks 2026",
+    position: "50% 40%",
+  },
 ];
 
 /**
